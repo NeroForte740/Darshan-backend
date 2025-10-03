ed6599a5ab50bb678e5e5d82301fa5283fc1ef31
NeroForte740 <kelvindasilva@edu.unifor.br>
2025-10-03 20:12:17 -0300

    'merge'

---
+++ project/src/app/controllers/FuncionariosController.js
@@ -0,0 +1,53 @@
+const FuncionariosModel = require ('../model/FuncionariosModel');
+
+class FuncionariosController {
+
+  async index(req, res) {
+    try {
+      const data = await FuncionariosModel.findAll();
+      return res.send(data);
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const id = parseInt(req.params.id);
+      const data = await FuncionariosModel.findById(id);
+      return res.send(data);
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const data = await FuncionariosModel.create(req.body);
+      return res.send(data);
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const id = parseInt(req.params.id);
+      const data = await FuncionariosModel.update(id, req.body);
+      return res.send(data);
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
+
+  async destroy(req, res) {
+    try {
+      const id = parseInt(req.params.id);
+      await FuncionariosModel.delete(id);
+      return res.send("Status 201");
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
+}
+module.exports = new FuncionariosController();
---
+++ project/src/app/model/PedidosModel.js
@@ -0,0 +1,110 @@
+const dotenv = require("dotenv").config();
+const cliente = require("../../config/db");
+
+class PedidoModel {
+  async findAll() {
+    const { data, error } = await cliente.supabase.from("pedidos").select(`
+        *,
+        pedido_produto (
+          quantidade,
+          produtos (*)
+        )
+      `);
+    if (error) throw error;
+    return data;
+  }
+
+  async findById(id) {
+    const { data, error } = await cliente.supabase
+      .from("pedidos")
+      .select(
+        `
+        *,
+        pedido_produto (
+          quantidade,
+          produtos (*)
+        )
+      `
+      )
+      .eq("ped_id", id)
+      .single();
+    if (error) throw error;
+    return data;
+  }
+
+  async create(pedido) {
+    const {
+      description,
+      totalprice,
+      status_preparo,
+      status_pag,
+      client,
+      priority,
+    } = pedido;
+
+    // Cria o pedido
+    const { data: newPedido, error: pedidoError } = await cliente.supabase
+      .from("pedidos")
+      .insert([
+        {
+          ped_description: description,
+          ped_totalprice: totalprice,
+          ped_status_preparo: status_preparo,
+          ped_status_pag: status_pag,
+          ped_client: client,
+          ped_priori: priority,
+        },
+      ])
+      .select()
+      .single();
+
+    if (pedidoError) throw pedidoError;
+
+    return newPedido;
+  }
+
+  async update(id, pedido) {
+    if (!pedido) {
+      throw new Error("O objeto 'pedido' não foi fornecido.");
+    }
+    // Atualiza os dados do pedido
+    const {
+      description,
+      totalprice,
+      status_preparo,
+      status_pag,
+      client,
+      priority,
+    } = pedido;
+
+    const { data: updatedPedido, error: pedidoError } = await cliente.supabase
+      .from("pedidos")
+      .update({
+        ped_description: description,
+        ped_totalprice: totalprice,
+        ped_status_preparo: status_preparo,
+        ped_status_pag: status_pag,
+        ped_client: client,
+        ped_priori: priority,
+      })
+      .eq("ped_id", id)
+      .select()
+      .single();
+
+    if (pedidoError) throw pedidoError;
+
+    return updatedPedido;
+  }
+
+  async delete(id) {
+    const { data, error } = await cliente.supabase
+      .from("pedidos")
+      .delete()
+      .eq("ped_id", id);
+
+    if (error) throw error;
+    return data;
+  }
+}
+
+module.exports = new PedidoModel();
